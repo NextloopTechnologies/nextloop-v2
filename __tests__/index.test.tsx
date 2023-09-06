b473b84048b01e0ae4a2e10fad69c62cf3c2858b
rakshitvermaa7 <rakshit@qureos.com>
2023-09-06 18:36:04 +0530

    inetial commit

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import Home from '../src/pages/index';
+
+describe('Home', () => {
+  it('dummy test that always passes', () => {
+    render(<Home />);
+  });
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className='w-full h-screen bg-red-200 text-3xl'>Here </div>
+    </>
+  );
+}
