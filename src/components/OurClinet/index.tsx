d6bcbaff6000634756131795a73684052224b6ef
mdnizam-nextloop <mdnizam@nextlooptechnologies.com>
2024-10-22 20:04:26 +0530

    Changes for font size issue

--- src/components/OurClinet/index.tsx
+++ src/components/OurClinet/index.tsx
@@ -59,7 +59,7 @@ const OurCLients: FC = () => {
         <header className='flex gap-10 lg:w-4/6 lg:px-10 text-center'>
           <div className='pt-2 w-full flex flex-col justify-center items-center gap-y-3 z-10 px-4'>
             <div
-              className={`${palette.fontSize.heading2.mobile} md:${palette.fontSize.heading2.desktop} text-white uppercase font-bold text-center`}
+              className={`${palette.fontSize.heading2.mobile} md:text-4xl 2xl:text-6xl text-white uppercase font-bold text-center`}
             >
               OUR <span className='text-orange-500'>Clients</span>
             </div>
