a414f8639cc6479d09cb62b0562b78bb4fee1ca5
suyashkumarpandey <spandey@nextlooptechnologies.com>
2024-12-07 01:15:31 +0530

    Changes for seo optimisation -- Single h1 tag in home page -- Sitemap file added -- Optimised ecommerce page it dynamic import load -- aria attributes added to popup

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -125,7 +125,13 @@ const Header: React.FC<HeaderProps> = ({ isSticky, headerColor }) => {
               onMouseEnter={() => setShowIndustriesDropdown(true)}
               onMouseLeave={() => setShowIndustriesDropdown(false)}
             >
-              <div>Industries</div>
+              <div
+                className='cursor-pointer'
+                role='button'
+                aria-haspopup='true'
+              >
+                Industries
+              </div>
               {showIndustriesDropdown && (
                 <ul className='absolute left-0 mt-0 w-48 rounded-2xl rounded-tl-none shadow-lg bg-black ring-1 ring-black ring-opacity-5 py-2 pr-2 border border-orange-500 space-y-3'>
                   {industries.map((industry) => (
