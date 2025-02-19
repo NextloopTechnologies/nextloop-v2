18e394478c74a24a9f0252b4654039a12d7306f3
suyashkumarpandey <spandey@nextlooptechnologies.com>
2025-02-19 12:55:43 +0530

    ADD: Added svg for error 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 
+import { Error404Svg } from '../../assets/SvgExport';
+
 const Layout = dynamic(() => import('../components/Layout/Layout'), {
   ssr: false,
 });
@@ -9,11 +11,12 @@ export default function Custom404() {
   return (
     <>
       <Layout>
-        <div className='flex flex-col items-center justify-center h-screen text-center bg-gray-100'>
-          <h1 className='text-4xl font-bold text-gray-800 mb-4'>
+        <div className='flex flex-col items-center justify-center h-screen text-center bg-gray-400 py-16'>
+          <Error404Svg />
+          <h2 className='text-4xl font-bold text-white mb-4'>
             404 - Page Not Found
-          </h1>
-          <p className='text-lg text-gray-600 mb-8'>
+          </h2>
+          <p className='text-lg text-white mb-8'>
             Oops! The page you’re looking for doesn’t exist or has been moved.
           </p>
           <div className='flex flex-col space-y-4'>
@@ -22,18 +25,6 @@ export default function Custom404() {
               className='px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700'
             >
               Back to Home
-            </Link>
-            <Link
-              href='/about-us'
-              className='px-6 py-3 bg-gray-200 text-gray-800 rounded-lg shadow hover:bg-gray-300'
-            >
-              About Us
-            </Link>
-            <Link
-              href='/blog'
-              className='px-6 py-3 bg-gray-200 text-gray-800 rounded-lg shadow hover:bg-gray-300'
-            >
-              Blog
             </Link>
           </div>
         </div>
