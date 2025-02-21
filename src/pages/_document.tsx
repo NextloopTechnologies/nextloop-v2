a1fa9b8572b8832d16ca8636805c90acc4d27450
suyashkumarpandey <spandey@nextlooptechnologies.com>
2025-02-21 20:24:31 +0530

    ADD: Added JSON-LD for SEO and integrates Google Tag Manager and Apollo tracking scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,12 @@ export default function Document() {
           name='google-site-verification'
           content='1ZvPKSWx3wAqnYNwsBJFWw-0JipRSjhH68LI0Gxs8J4'
         ></meta>
+        <script>
+          {`function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+          o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+          o.onload=function(){window.trackingFunctions.onLoad({appId:"6799e3966d69e201b08953e1"})},
+          document.head.appendChild(o)}initApollo();`}
+        </script>
       </Head>
       <body>
         <noscript
