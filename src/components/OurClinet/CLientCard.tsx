0cc78da708a552035d79a494719810cd4b37fb1e
priteshNextloop <pbhati@nextlooptechnologies.com>
2025-08-01 23:21:51 +0530

    fix: dynamic blogs and slider on homepage

--- src/components/OurClinet/CLientCard.tsx
+++ src/components/OurClinet/CLientCard.tsx
@@ -8,10 +8,8 @@ interface ServiceCardProps {
 
 const CLientCard: FC<ServiceCardProps> = ({ image }) => {
   return (
-    <div className='max-w-sm mx-auto bg-white border border-border rounded-[24px] shadow-lg p-2'>
-      <div className='flex justify-center items-center w-[110px] h-[110px] 2xl:w-[150px] 2xl:h-[150px] rounded-[50%]'>
-        <Image src={image} alt='image' className='w-full object-contain' />
-      </div>
+    <div className='flex justify-center items-center max-w-sm mx-auto bg-white border border-border rounded-[24px] shadow-lg p-2 w-[110px] h-[110px] 2xl:w-[150px] 2xl:h-[150px]'>
+      <Image src={image} alt='image' className='w-full object-contain' />
     </div>
   );
 };
