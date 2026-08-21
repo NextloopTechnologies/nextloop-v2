68e4dcd6ee686d49c1b7aaf0988802e58c864bda
DivyaNextloop <dbilthare@nextlooptechnologies.com>
2026-08-21 11:35:29 -0400

    fix: build failure

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,5 +3,10 @@ import { StaticImageData } from 'next/image';
 export const getStaticImageData = (url: string): StaticImageData =>
   url as unknown as StaticImageData;
 
-export const toTitleCase = (text: string = '') =>
-  text.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+export const toTitleCase = (text = ''): string => {
+  if (typeof text !== 'string' || !text.trim()) {
+    return '';
+  }
+
+  return text.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+};
